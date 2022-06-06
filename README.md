edb393e62607451f3cd5f766d15e509404423dc7
Bea Stollnitz <bea@stollnitz.com>
2022-06-06 16:41:19 -0700

    Adding initial batch endpoint code.

---
+++ README.md
@@ -0,0 +1,91 @@
+This project shows how to deploy a Fashion MNIST MLflow model using an online managed endpoint. Endpoint 1 demonstrates the simplest scenario, and endpoint 2 demonstrates how to wrap the MLflow model with custom code.
+
+## Azure setup
+
+* You need to have an Azure subscription. You can get a [free subscription](https://azure.microsoft.com/en-us/free?WT.mc_id=aiml-44166-bstollnitz) to try it out.
+* Create a [resource group](https://docs.microsoft.com/en-us/azure/azure-resource-manager/management/manage-resource-groups-portal?WT.mc_id=aiml-44166-bstollnitz).
+* Create a new machine learning workspace by following the "Create the workspace" section of the [documentation](https://docs.microsoft.com/en-us/azure/machine-learning/quickstart-create-resources?WT.mc_id=aiml-44166-bstollnitz). Keep in mind that you'll be creating a "machine learning workspace" Azure resource, not a "workspace" Azure resource, which is entirely different!
+* If you have access to GitHub Codespaces, click on the "Code" button in this GitHub repo, select the "Codespaces" tab, and then click on "New codespace."
+* Alternatively, if you plan to use your local machine:
+  * Install the Azure CLI by following the instructions in the [documentation](https://docs.microsoft.com/en-us/cli/azure/install-azure-cli?WT.mc_id=aiml-44166-bstollnitz).
+  * Install the ML extension to the Azure CLI by following the "Installation" section of the [documentation](https://docs.microsoft.com/en-us/azure/machine-learning/how-to-configure-cli?WT.mc_id=aiml-44166-bstollnitz).
+* In a terminal window, login to Azure by executing `az login --use-device-code`. 
+* Set your default subscription by executing `az account set -s "<YOUR_SUBSCRIPTION_NAME_OR_ID>"`. You can verify your default subscription by executing `az account show`, or by looking at `~/.azure/azureProfile.json`.
+* Set your default resource group and workspace by executing `az configure --defaults group="<YOUR_RESOURCE_GROUP>" workspace="<YOUR_WORKSPACE>"`. You can verify your defaults by executing `az configure --list-defaults` or by looking at `~/.azure/config`.
+* You can now open the [Azure Machine Learning studio](https://ml.azure.com/?WT.mc_id=aiml-44166-bstollnitz), where you'll be able to see and manage all the machine learning resources we'll be creating.
+* Although not essential to run the code in this post, I highly recommend installing the [Azure Machine Learning extension for VS Code](https://marketplace.visualstudio.com/items?itemName=ms-toolsai.vscode-ai).
+
+
+
+## Project setup
+
+If you have access to GitHub Codespaces, click on the "Code" button in this GitHub repo, select the "Codespaces" tab, and then click on "New codespace".
+
+Alternatively, you can set up your local machine using the following steps.
+
+Install conda environment:
+
+```
+conda env create -f environment.yml
+```
+
+Activate conda environment:
+
+```
+conda activate aml-online-endpoint-mlflow
+```
+
+
+# Train locally
+
+Open the 'endpoint-1/src/train.py` file and press F5. An 'endpoint-1/model' is created with the trained model. Repeat for endpoint 2.
+
+
+# Deploy in the cloud
+
+## Endpoint 1
+
+```
+cd aml-online-endpoint-mlflow/endpoint-1
+```
+
+```
+mlflow models predict --model-uri model --input-path "../test-data/images.csv" --content-type csv
+```
+
+```
+az ml model create --path model/ --name model-online-mlflow-1 --version 1 
+```
+
+```
+az ml online-endpoint create -f cloud/endpoint.yml
+az ml online-deployment create -f cloud/deployment.yml --all-traffic
+```
+
+```
+az ml online-endpoint invoke --name endpoint-online-mlflow-1 --request-file ../test-data/images_azureml.json
+```
+
+
+## Endpoint 2
+
+```
+cd aml-online-endpoint-mlflow/endpoint-2
+```
+
+```
+mlflow models predict --model-uri pyfunc-model --input-path "../test-data/images.csv" --content-type csv
+```
+
+```
+az ml model create --path pyfunc-model/ --name model-online-mlflow-2 --version 1 
+```
+
+```
+az ml online-endpoint create -f cloud/endpoint.yml
+az ml online-deployment create -f cloud/deployment.yml --all-traffic
+```
+
+```
+az ml online-endpoint invoke --name endpoint-online-mlflow-2 --request-file ../test-data/images_azureml.json
+```
---
+++ aml-online-endpoint-mlflow/endpoint-2/src/train.py
@@ -0,0 +1,139 @@
+"""Training and evaluation."""
+
+import argparse
+import logging
+import shutil
+from pathlib import Path
+from typing import Tuple
+
+import mlflow
+import torch
+from torch import nn
+from torch.utils.data import DataLoader, random_split
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+
+from common import ARTIFACT_NAME
+from model_wrapper import ModelWrapper
+from neural_network import NeuralNetwork
+from utils_train_nn import evaluate, fit
+
+DATA_DIR = "aml-online-endpoint-mlflow/data"
+PYTORCH_MODEL_DIR = "aml-online-endpoint-mlflow/endpoint-2/pytorch-model"
+PYFUNC_MODEL_DIR = "aml-online-endpoint-mlflow/endpoint-2/pyfunc-model"
+
+
+def load_train_val_data(
+        data_dir: str, batch_size: int,
+        training_fraction: float) -> Tuple[DataLoader, DataLoader]:
+    """
+    Returns two DataLoader objects that wrap training and validation data.
+    Training and validation data are extracted from the full original training
+    data, split according to training_fraction.
+    """
+    full_train_data = datasets.FashionMNIST(data_dir,
+                                            train=True,
+                                            download=True,
+                                            transform=ToTensor())
+    full_train_len = len(full_train_data)
+    train_len = int(full_train_len * training_fraction)
+    val_len = full_train_len - train_len
+    (train_data, val_data) = random_split(dataset=full_train_data,
+                                          lengths=[train_len, val_len])
+    train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
+    val_loader = DataLoader(val_data, batch_size=batch_size, shuffle=True)
+
+    return (train_loader, val_loader)
+
+
+def save_model(pytorch_model_dir: str, pyfunc_model_dir: str,
+               model: nn.Module) -> None:
+    """
+    Saves the trained model.
+    """
+    # Save PyTorch model.
+    pytorch_code_filenames = ["neural_network.py", "utils_train_nn.py"]
+    pytorch_full_code_paths = [
+        Path(Path(__file__).parent, code_path)
+        for code_path in pytorch_code_filenames
+    ]
+    logging.info("Saving PyTorch model to %s", pytorch_model_dir)
+    shutil.rmtree(pytorch_model_dir, ignore_errors=True)
+    mlflow.pytorch.save_model(pytorch_model=model,
+                              path=pytorch_model_dir,
+                              code_paths=pytorch_full_code_paths)
+
+    # Save PyFunc model that wraps the PyTorch model.
+    pyfunc_code_filenames = ["model_wrapper.py", "common.py"]
+    pyfunc_full_code_paths = [
+        Path(Path(__file__).parent, code_path)
+        for code_path in pyfunc_code_filenames
+    ]
+    model = ModelWrapper()
+    artifacts = {
+        ARTIFACT_NAME: pytorch_model_dir,
+    }
+    conda_env = Path(pytorch_model_dir, "conda.yaml").as_posix()
+    logging.info("Saving PyFunc model to %s", pyfunc_model_dir)
+    shutil.rmtree(pyfunc_model_dir, ignore_errors=True)
+    mlflow.pyfunc.save_model(path=pyfunc_model_dir,
+                             python_model=model,
+                             artifacts=artifacts,
+                             code_path=pyfunc_full_code_paths,
+                             conda_env=conda_env)
+
+
+def train(data_dir: str, pytorch_model_dir: str, pyfunc_model_dir: str,
+          device: str) -> None:
+    """
+    Trains the model for a number of epochs, and saves it.
+    """
+    learning_rate = 0.1
+    batch_size = 64
+    epochs = 5
+
+    (train_dataloader,
+     val_dataloader) = load_train_val_data(data_dir, batch_size, 0.8)
+    model = NeuralNetwork()
+    loss_fn = nn.CrossEntropyLoss()
+    optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+
+    for epoch in range(epochs):
+        logging.info("Epoch %d", epoch + 1)
+        (training_loss, training_accuracy) = fit(device, train_dataloader,
+                                                 model, loss_fn, optimizer)
+        (validation_loss,
+         validation_accuracy) = evaluate(device, val_dataloader, model, loss_fn)
+
+        metrics = {
+            "training_loss": training_loss,
+            "training_accuracy": training_accuracy,
+            "validation_loss": validation_loss,
+            "validation_accuracy": validation_accuracy
+        }
+        mlflow.log_metrics(metrics, step=epoch)
+
+    save_model(pytorch_model_dir, pyfunc_model_dir, model)
+
+
+def main():
+    logging.basicConfig(level=logging.INFO)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--data_dir", dest="data_dir", default=DATA_DIR)
+    parser.add_argument("--pytorch_model_dir",
+                        dest="pytorch_model_dir",
+                        default=PYTORCH_MODEL_DIR)
+    parser.add_argument("--pyfunc_model_dir",
+                        dest="pyfunc_model_dir",
+                        default=PYFUNC_MODEL_DIR)
+    args = parser.parse_args()
+    logging.info("input parameters: %s", vars(args))
+
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+
+    train(**vars(args), device=device)
+
+
+if __name__ == "__main__":
+    main()
